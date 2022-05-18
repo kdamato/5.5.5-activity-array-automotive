d1e015176321d891f3aff1614cece09f33048d6f
Kyle DAmato <kyledamato4@gmail.com>
2022-05-18 13:56:46 -0400

    finished assignment

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,8 +1,60 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require('./vehicle').Vehicle
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
 
-import "vehicle.js";
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log(`vehicle is starting.`);
+            return this.started = true
+        } else {
+            console.log(`Gas tank empty`);
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(`A ${this.model} ${this.make} can't fit all passengers.`);
+
+            }
+        } else {
+            console.log(`A ${this.model} ${this.make} is full`);
+        }
+    }
+
+
+}
+
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
